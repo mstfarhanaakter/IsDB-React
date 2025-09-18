d44bdab295878211ee96341a418ce5b81e56af22
mstfarhanaakter <farhanashetu.idb@gmail.com>
2025-09-18 12:08:16 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+
+import './App.css'
+import Header from './header.jsx'
+
+function App() {
+//  const loggedIn = true;
+ 
+ 
+return(
+  <div>
+  <Header />  
+  <h1 className='bg-red-500 text-white'>Vite+React</h1>
+  
+  <h1>headerr</h1>
+  <h1>nav </h1>
+  <h1>main</h1>
+  <h1>footer</h1>
+
+  <p>this is a paragraph</p>
+  <p>this is a paragraph</p>
+  <p>this is a paragraph</p>
+  </div>
+  
+  
+  
+  
+)
+      
+      
+     
+}
+
+export default App
