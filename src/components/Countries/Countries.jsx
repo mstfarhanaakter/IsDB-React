067253c37cbbf3b3ba09861b018ceca3cdce37a0
mstfarhanaakter <farhanashetu.idb@gmail.com>
2025-09-19 23:55:42 +0600

    new

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -31,14 +31,14 @@ const Countries = ({countriesPromise}) => {
         <div>
             <h1 className='text-[12px]'>Total Countries: {data.length} </h1>
             <p>Total Country Visited: {visitedCountries.length} </p>
-            <ol className='list-decimal'>
+            {/* <ol className='list-decimal'>
                 {
                     visitedCountries.map(country => <li>{country.name.common}</li>)
                 }
                 
-            </ol>
+            </ol> */}
             
-                <p>Total Flag Visited: </p>
+                <p>Total Flag Visited: {vistitedFlags.length} </p>
             <div className='w-[100px] flex gap-3'>
             {
                 vistitedFlags.map((flag, index) => <img key={index} src={flag} /> )
@@ -48,7 +48,8 @@ const Countries = ({countriesPromise}) => {
             <div className='grid grid-cols-3 gap-4 p-2'>
         {
             data.map((country) => 
-            <Country 
+            <Country
+            key = {country.cca3.cca3} 
             country={country}
             handleVisitedCountries = {handleVisitedCountries}
             handlevistiedFlags = {handlevistiedFlags}
