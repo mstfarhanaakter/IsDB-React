7d02f268fcd131b331d053247902c1f218b04818
mstfarhanaakter <farhanashetu.idb@gmail.com>
2025-09-22 23:28:36 +0600

    new

--- tsp-routing/src/components/Nav/Nav.jsx
+++ tsp-routing/src/components/Nav/Nav.jsx
@@ -3,10 +3,18 @@ import { Link } from 'react-router-dom';
 
 const Nav = () => {
     return (
-        <div>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About Us</Link></li>
-            <li><Link to="/Contact">Contact Us</Link></li>
+        <div className='flex gap-6 p-5 bg-gray-500 text-white font-bold justify-end text-right  '>
+            <div className='flex justify-start'>
+                <img src="/src/assets/img/pizza.png" alt="Pizza" className='w-[25px] h-[25px]' />
+            </div>
+            
+            <span><Link to="/">Home</Link></span>
+            <span><Link to="/about">Menu</Link></span>
+            <span><Link to="/Contact">Services</Link></span>
+            <span><Link to="/Contact">Blog</Link></span>
+            <span><Link to="/about">About</Link></span>
+            <span><Link to="/Contact">Contact</Link></span>
+
             
 
         </div>
