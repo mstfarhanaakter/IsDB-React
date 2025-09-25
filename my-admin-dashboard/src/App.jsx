55fbef7d5bd52886f78d1d6bd7994b903a14a991
mstfarhanaakter <farhanashetu.idb@gmail.com>
2025-09-25 22:11:56 +0600

    admin

---
+++ my-admin-dashboard/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Sidebar from './components/Layout/Sidebar';
+
+const App = () => {
+  return (
+    <div>
+      <Sidebar/>
+    </div>
+  );
+};
+
+export default App;
